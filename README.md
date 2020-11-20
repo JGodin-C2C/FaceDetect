df6a34dfd00c4ec60e83af898e1e1ad2756b02da
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-11-20 13:44:36 +0100

    Output the mustached frames in a virtual camera
    Use pyfakewebcam and v4l2loopback to create a /dev/video2 camera,
    the frames are output there. It is more convenient to use directly
    in meeting (web) applications.

--- README.md
+++ README.md
@@ -8,7 +8,30 @@ Get fancy on these zoom meetings !
 
 # How-to.
 
+Install and load v4l2loopback to create a virtual /dev/video2
+
+
 ```
-pip install opencv-python
+apt-get install v4l2loopback-utils
+modprobe v4l2loopback devices=1  exclusive_caps=1 video_nr=2 card_label="mustache-mustache"
+```
+
+⚠️ Warning ⚠️: on installation , `v4loopback` will modprobe at startup and the
+exclusive_caps=1 may be lost.  you have to onload it first with:
+
+```
+modprobe -r v4l2loopback  
+```
+
+Python dependencies
+
+```
+pip install opencv-python pyfakewebcam
+```
+
+Run
+
+```
 python live.py
 ```
+
--- live.py
+++ live.py
@@ -3,50 +3,64 @@
 
 import cv2
 import numpy as np
+
+import pyfakewebcam
+
+
 cap = cv2.VideoCapture(0)
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 
 # Create the haar cascade
 faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-while(True):
-  # Capture frame-by-frame
-  ret, frame = cap.read()
-  b_channel, g_channel, r_channel = cv2.split(frame)
-  alpha_channel = np.ones(b_channel.shape, dtype=b_channel.dtype) * 50 #creating a dummy alpha channel image.
-  frame = cv2.merge((b_channel, g_channel, r_channel, alpha_channel))
+# setup the fake camera
+fake = pyfakewebcam.FakeWebcam('/dev/video2', width, height)
 
-  # Our operations on the frame come here
-  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+mustache = cv2.imread('mustache.png', cv2.IMREAD_UNCHANGED)
 
-  # Detect faces in the image
-  faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(30, 30)
-    #flags = cv2.CV_HAAR_SCALE_IMAGE
-  )
+def get_frame(cap):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    b_channel, g_channel, r_channel = cv2.split(frame)
+    alpha_channel = np.ones(b_channel.shape, dtype=b_channel.dtype) * 50 #creating a dummy alpha channel image.
+    frame = cv2.merge((b_channel, g_channel, r_channel, alpha_channel))
 
-  mustache = cv2.imread('mustache.png', cv2.IMREAD_UNCHANGED)
-  alpha_s = mustache[:,:,3]/255.0 # extract it
-  alpha_l = 1.0 - alpha_s   # invert b/w
-  # extract size
-  (wH, wW) = mustache.shape[:2]
-  xoffset = 20
-  yoffset = 90
-# Draw for each layer
-  for (x, y, w, h) in faces:
-    for c in range(0,3):
-      try:
-        frame[y + yoffset :y + wH + yoffset, x + xoffset  :x + wW + xoffset , c] =  (alpha_s * mustache[:, :, c] + alpha_l * frame[y  + yoffset :y + wH + yoffset, x + xoffset :x + wW + xoffset, c])
-      except:
-        continue
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-  # Display the resulting frame
-  cv2.imshow('frame', frame)
-  if cv2.waitKey(1) & 0xFF == ord('q'):
-    break
+    # Detect faces in the image
+    faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30)
+            #flags = cv2.CV_HAAR_SCALE_IMAGE
+            )
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+    alpha_s = mustache[:,:,3]/255.0 # extract it
+    alpha_l = 1.0 - alpha_s   # invert b/w
+    # extract size
+    (wH, wW) = mustache.shape[:2]
+    xoffset = 20
+    yoffset = 90
+    # Draw for each layer
+    for (x, y, w, h) in faces:
+        for c in range(0,3):
+            try:
+                frame[y + yoffset :y + wH + yoffset, x + xoffset  :x + wW + xoffset , c] =  (alpha_s * mustache[:, :, c] + alpha_l * frame[y  + yoffset :y + wH + yoffset, x + xoffset :x + wW + xoffset, c])
+            except:
+                continue
+
+    return frame
+
+
+if __name__ == '__main__':
+    print('Running...')
+    print('Please press CTRL-C to exit.')
+    # frames forever
+    while True:
+        frame = get_frame(cap)
+        # fake webcam expects RGB
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        fake.schedule_frame(frame)
